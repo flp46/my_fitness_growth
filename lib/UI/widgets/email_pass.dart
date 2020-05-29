42bc1b272eb4de80afc8b7ad9d5ea38804d68294
flp46 <alarconnieto@gmail.com>
2020-05-28 21:14:24 -0500

    mostar y ocultar el password

--- lib/UI/widgets/email_pass.dart
+++ lib/UI/widgets/email_pass.dart
@@ -2,9 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:my_fitness_growth/Usuario/Bloc/user_bloc.dart';
 
+class ShowHidePass extends ChangeNotifier{
+
+  bool binary = true;
+
+  void changeBinary(){
+    binary = !binary;
+    notifyListeners();
+  }
+
+}
+
+
+
 class LoginEmail extends StatelessWidget{
 
   UserBloc userBloc;
+  final showHide = ShowHidePass();
 
   @override
   Widget build(BuildContext context) {
@@ -16,48 +30,63 @@ class LoginEmail extends StatelessWidget{
     final passController = TextEditingController();
 
     // TODO: implement build
-    return Container(
-      width: widthScreen,
-      child: Column(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelStyle: TextStyle(
-                color: Colors.white70
+    return AnimatedBuilder(
+      animation: showHide,
+      builder: (_, __) => Container(
+          width: widthScreen,
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(
+                    color: Colors.white70
+                  ),
+                  hintText: 'sasha@hotmail.com',
+                  labelText: 'Correo',
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white70)
+                  )
+                  
+                ),
+                keyboardType: TextInputType.emailAddress,
+                controller: emailController,
               ),
-              hintText: 'sasha@hotmail.com',
-              labelText: 'Correo',
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.white70)
-              )
-              
-            ),
-            keyboardType: TextInputType.emailAddress,
-            controller: emailController,
-          ),  
-          TextField(
-            decoration: InputDecoration(
-              labelStyle: TextStyle(
-                color: Colors.white70
-              ),
-              labelText: 'Contraseña',
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.white70)
-              )
-              
-            ),
-            keyboardType: TextInputType.text,
-            controller: passController,
+              Stack(
+                alignment: Alignment.bottomRight,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        color: Colors.white70
+                      ),
+                      labelText: 'Contraseña',
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white70)
+                      ),
+                      // suffixIcon: Icon(Icons.visibility)
+                    ),
+                    keyboardType: TextInputType.text,
+                    controller: passController,
+                    obscureText: showHide.binary,
+                  ),
+                  IconButton(
+                    icon: showHide.binary ? Icon(Icons.visibility) : Icon(Icons.visibility_off), 
+                    onPressed: (){
+                      showHide.changeBinary();
+                    }
+                  )
+                ],
+              ),  
+              RaisedButton(
+                onPressed: (){
+                  print(emailController.text);
+                  userBloc.register(emailController.text, passController.text);
+                },
+                child: Text('Form'),
+              )       
+            ]
           ),
-          RaisedButton(
-            onPressed: (){
-              print(emailController.text);
-              userBloc.register(emailController.text, passController.text);
-            },
-            child: Text('Form'),
-          )       
-        ]
-      ),
+        )
     );
   }
   
