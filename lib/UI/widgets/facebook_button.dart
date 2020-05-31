80a712ac0f5c0984ccadfd5bcac9f8a0c42454f6
flp46 <alarconnieto@gmail.com>
2020-05-30 19:55:17 -0500

    Configuracion y funcionalidad de registro con facebook

---
+++ lib/UI/widgets/facebook_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:my_fitness_growth/UI/widgets/snackbar_error.dart';
+import 'package:my_fitness_growth/Usuario/Bloc/user_bloc.dart';
+
+class ControlButton extends ChangeNotifier{
+
+  bool isPressed = true;
+
+  //CONTROLA SI SE HACE CLICK MUY RAPIDO EN EL BOTON DE REGISTRARME
+  void changeBinaryButton(){
+    isPressed = !isPressed;
+    notifyListeners();
+  }
+
+}
+
+
+
+
+
+class FacebookButton extends StatelessWidget{
+
+  UserBloc userBloc;
+  final controlButton = ControlButton();
+
+  @override
+  Widget build(BuildContext context) {
+
+    userBloc = BlocProvider.of(context);
+
+    // TODO: implement build
+    return FloatingActionButton(
+      backgroundColor: Color(0xFF3b5998),
+      child: FaIcon(FontAwesomeIcons.facebookF),
+      onPressed: (){
+        if (controlButton.isPressed == true){
+          controlButton.changeBinaryButton();
+          userBloc.loginWithFacebook().then((value){
+            if (value != null){
+              Scaffold.of(context).showSnackBar(SnackbarError(errorMessage: value).showError());
+              controlButton.changeBinaryButton();
+            } else {
+              print('No tan rapido');
+            }
+          });
+        }
+      },
+    );
+  }
+
+}
